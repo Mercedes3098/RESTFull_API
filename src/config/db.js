079a55a9dfe486b7c5d8c67dc59793e0b07e9b31
Mercedes Lagardo <mercedeslagardo@gmail.com>
2025-08-11 15:28:38 -0300

    modificación

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise"
 export const pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "admin",
+    password: "admi",
     database: "biblioteca",
     port: 3300,
     waitForConnections: true,
